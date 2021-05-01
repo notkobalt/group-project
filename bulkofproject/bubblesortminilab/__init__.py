91afb2d0fa4fd392b1e3888f7cc63c492864b360
dylanroman <dylanroman143@gmail.com>
2021-04-30 21:09:56 -0700

    bubblesort add

--- bulkofproject/bubblesortminilab/__init__.py
+++ bulkofproject/bubblesortminilab/__init__.py
@@ -1,9 +1,17 @@
 #import flask and blueprints
 from flask import Blueprint, render_template, __name__, request
+#import dylan lab
+from bulkofproject.bubblesortminilab.dylanlab import listgen, bubblesort
 
 #create blueprint
 bubblesortminilab = Blueprint('bubble_sort', __name__)
 
 @bubblesortminilab.route('/dylan', methods = ['GET', 'POST'])
 def dylan():
-    return render_template('bubblesortminilab/dylan.html')
+    #list
+    initial = listgen(10)
+    print(initial)
+    final = bubblesort(initial)
+    print(final)
+
+    return render_template('bubblesortminilab/dylan.html', initial = initial, final = final)
