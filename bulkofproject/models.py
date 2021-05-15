4894ba77cc9d182c190e43851575c9d42d375520
dylanroman <dylanroman143@gmail.com>
2021-05-14 19:22:50 -0700

    Added "rating" database models and conencted it to users database

--- bulkofproject/models.py
+++ bulkofproject/models.py
@@ -1,6 +1,7 @@
 #imports
 ##import config from __init__
 from bulkofproject import db
+from datetime import datetime
 
 #database tables (models)
 ##login db
@@ -9,7 +10,21 @@ class users(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(15), unique = True, nullable = False)
     password = db.Column(db.String(), nullable = False)
+    #link to rating db
+    ratings = db.relationship('rating', backref = 'author', lazy = True)
 
     #callable attributes
     def __repr__(self):
         return f"user('{self.username}', '{self.password}')"
+
+##rating db
+class rating(db.Model):
+    #table columns
+    id = db.Column(db.Integer, primary_key = True)
+    stars = db.Column(db.Integer, nullable = False)
+    review = db.Column(db.Text, nullable = False)
+    #link to users db
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def __repr__(self):
+        return f"user('{self.stars}', '{self.review}')"
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # imports
 ##import config from __init__
 from bulkofproject import app
+from bulkofproject import db
 
 # run project
 if __name__ == '__main__' :
