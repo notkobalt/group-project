7a480a87a938efe91e559a50eabcf3916055590b
dylanroman <dylanroman143@gmail.com>
2021-06-03 10:29:38 -0700

    created reviews blueprint, created reviews adaptable routes, started API query

--- bulkofproject/reviews/__init__.py
+++ bulkofproject/reviews/__init__.py
@@ -3,10 +3,16 @@ from flask import Blueprint, render_template, __name__, request
 
 reviews_bp = Blueprint('reviews', __name__)
 
-@reviews_bp.route('/<game>')
+#game = game
+game = 'test'
+@reviews_bp.route('/<link>')
 #parameters in "reviews" make the link adaptable, will have a for loop once query system is set up changein the 'test' text to the corresponding games (2 week ticket difficult to convey in 1 ticket a week system)
-def reviews(game = 'test'):
-    return render_template('reviews/reviews.html')
+
+#for game in gamelist:
+def reviews(link = game):
+
+    return render_template('reviews/reviews.html', #game = test
+    )
 
 
 
